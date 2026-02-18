3b37085aeb20009395a258be08c485110eecc530
MatteoPr0 <passerinimatteo99@gmail.com>
2026-02-18 15:18:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const tabs = document.querySelectorAll('.tab');
 const modeChip = document.getElementById('device-mode');
 
 const updateDeviceMode = () => {
-  const isOpenFold = window.matchMedia('(min-width: 700px) and (min-aspect-ratio: 4/3)').matches;
+  const isOpenFold = window.matchMedia('(min-width: 700px)').matches;
   modeChip.textContent = isOpenFold ? 'Modalità: pieghevole aperto' : 'Modalità: smartphone chiuso';
 };
 
